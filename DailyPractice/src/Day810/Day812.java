7692360219878876f9161ea83a376441fa878889
Bee-le <1693332907@qq.com>
2021-08-28 16:52:40 +0800

    Day810

--- DailyPractice/src/Day810/Day812.java
+++ DailyPractice/src/Day810/Day812.java
@@ -4,5 +4,8 @@ public class Day812 {
     public static void main(String[] args) {
 
         System.out.println("adadada");
+        System.out.println("adadada");
+        System.out.println("adadada");
+        System.out.println("adadada");
     }
 }
