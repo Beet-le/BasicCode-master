aaf3023b8153d61e0a60f79055aeb8dd90944bc4
Bee-le <1693332907@qq.com>
2021-09-03 18:12:54 +0800

    Day810

--- DailyPractice/src/Day810/Day812.java
+++ DailyPractice/src/Day810/Day812.java
@@ -6,6 +6,5 @@ public class Day812 {
         System.out.println("adadada");
         System.out.println("adadada");
         System.out.println("adadada");
-        System.out.println("adadada");
     }
 }
